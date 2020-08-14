eb4e529b41fb69c09e0b1c5ffd9ff8f8b3019e48
urielquevedo <quevedouriel3@gmail.com>
2020-08-14 18:50:56 -0300

    creo una alerta para cuando no hay productos en el pedido

--- src/routes/confirmacion.svelte
+++ src/routes/confirmacion.svelte
@@ -7,6 +7,17 @@
   h1 {
     text-align: center;
   }
+  .alert {
+    background-color: #fff3cd;
+    color: #856404;
+    padding: .75rem 1.25rem;
+    border: 1px solid #ffeeba;
+    border-radius: .25rem;
+  }
+  a {
+    text-decoration: none;
+    color: blue;
+  }
 </style>
 
 <svelte:head>
@@ -17,4 +28,8 @@
 
 {#each [...$carrito.productosCotizados.values()] as producto}
   <Producto producto={producto} />
+  {:else}
+  <div class="alert">No hay ningun producto para confirmar, seleccione en
+    <a rel="prefetch" href="productos">Productos</a>.
+  </div>
 {/each}
